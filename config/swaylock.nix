888d6102ba8a28f87a20e7946a823475d92b792a
Jim Hargrave <jhargrave-straker@bitbucket.org>
2024-01-28 11:47:30 -0700

    more swaylock illlegal options

--- config/swaylock.nix
+++ config/swaylock.nix
@@ -3,23 +3,20 @@
   
 home.file.".config/swaylock/config".text = ''
     indicator-caps-lock
+    show-failed-attempts
     ignore-empty-password
     indicator-thickness=15
     indicator-radius=150
     image=~/Pictures/Wallpapers/scifi_landscape.jpg
-    effect-blur=7x5
-    effect-vignette=0.5:0.5
     ring-color=${config.colorScheme.colors.base0D}
     key-hl-color=${config.colorScheme.colors.base0F}
     line-color=00000000
     inside-color=00000088
     inside-clear-color=00000088
+    separator-color=00000000
     text-color=${config.colorScheme.colors.base05}
     text-clear-color=${config.colorScheme.colors.base05}
     ring-clear-color=${config.colorScheme.colors.base0D}
-    separator-color=00000000
-    grace=5
-    fade-in=0.5
     font=Ubuntu
   '';
 
