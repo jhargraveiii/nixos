d418a0d9df473a9bf46c43b940693b0fc6dbb111
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-03-16 13:23:31 -0600

    fix open-webui

--- modules/programs/open-webui.nix
+++ modules/programs/open-webui.nix
@@ -12,13 +12,13 @@
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers.open-webui = {
     autoStart = true;
-    image = "ghcr.io/open-webui/open-webui";
+    image = "ghcr.io/open-webui/open-webui:main";
     ports = [ "3000:8080" ];
     # TODO figure out how to create the data directory declaratively
-    volumes = [ "${config.users.users.jimh.home}/open-webui:/app/backend/data" ];
+    volumes = [ "${config.users.users.jimh.home}/DATA2/open-webui:/app/backend/data" ];
     extraOptions =
       [ "--network=host" "--add-host=host.containers.internal:host-gateway" ];
-    environment = { OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api"; };
+    environment = { OLLAMA_API_BASE_URL = "http://127.0.0.1:11434"; };
   };
   networking.firewall = { allowedTCPPorts = [ 80 443 8080 11434 3000 ]; };
 }
