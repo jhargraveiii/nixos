3c7cef09c8d6d7cfbd4ea25a472d376b7e02aa44
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-04-25 21:40:24 -0600

    override build flags

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -1,22 +1,17 @@
 { pkgs, lib, ... }:
 let
- ollamaOverride = pkgs.ollama.overrideAttrs (oldAttrs: rec {
-    version = "0.1.32";
-    src = pkgs.fetchFromGitHub {
-      owner = "jmorganca";
-      repo = "ollama";
-      rev = "v${version}";
-      hash = "sha256-Ip1zrhgGpeYo2zsN206/x+tcG/bmPJAq4zGatqsucaw="; 
-      fetchSubmodules = true;
-    };
-    NIX_CFLAGS_COMPILE = "-O3 -march=znver3 -mtune=znver3";
-    vendorHash = "sha256-Lj7CBvS51RqF63c01cOCgY7BCQeCKGu794qzb/S80C0="; 
-  });
+  ollamaOverride = pkgs.ollama.overrideAttrs
+    (oldAttrs: rec {
+      COMMON_CMAKE_DEFS =
+        "-DCMAKE_POSITION_INDEPENDENT_CODE=on -DLLAMA_NATIVE=on -DLLAMA_AVX=on -DLLAMA_AVX2=on -DLLAMA_AVX512=off -DLLAMA_FMA=on -DLLAMA_F16C=on";
+      NIX_CFLAGS_COMPILE = "-O3 -march=znver3 -mtune=znver3";
+    });
 in
 {
   systemd.services.ollama.serviceConfig.DynamicUser = lib.mkForce false;
 
   environment.systemPackages = with pkgs; [ ollamaOverride ];
+
   services.ollama = {
     enable = true;
     package = ollamaOverride;
