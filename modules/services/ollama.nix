3f4249999441675df3392d279d842064f4a84bd3
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-05-05 15:21:37 -0600

    attempts to optimize rollback

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -1,14 +1,12 @@
 { pkgs, lib, ... }:
 let
   ollamaOverride = pkgs.ollama.overrideAttrs (oldAttrs: rec {
-    COMMON_CMAKE_DEFS =
-      "-DCMAKE_POSITION_INDEPENDENT_CODE=on -DLLAMA_NATIVE=on -DLLAMA_AVX=on -DLLAMA_AVX2=on -DLLAMA_AVX512=off -DLLAMA_FMA=on -DLLAMA_F16C=on";
-    NIX_CFLAGS_COMPILE = "-O3 -march=znver3 -mtune=znver3";
+    NIX_CFLAGS_COMPILE = "-O3 -march=native -mtune=native";
   });
 in {
   systemd.services.ollama.serviceConfig.DynamicUser = lib.mkForce false;
 
-  environment.systemPackages = with pkgs; [ ollamaOverride oterm ];
+  environment.systemPackages = with pkgs; [ ollamaOverride ];
 
   services.ollama = {
     enable = true;
