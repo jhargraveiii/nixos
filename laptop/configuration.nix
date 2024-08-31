82bbbe0d3156fcaa7f01692e6a0d82aae04c76ce
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-08-31 13:55:02 -0600

    add tlp powermanagment config but disable for now

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -300,19 +300,29 @@
   powerManagement = {
     enable = true;
   };
-  services.power-profiles-daemon.enable = false;
+
+  services.tlp = {
+    enable = false;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+    };
+  };
+
+  services.power-profiles-daemon.enable = true;
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
-	  battery = {
-	     governor = "ondemand";
-	     turbo = "auto";
-	  };
-	  charger = {
-	     governor = "performance";
-	     turbo = "auto";
-	  };
-	};
-
+    battery = {
+      governor = "ondemand";
+      turbo = "auto";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
 
   hardware.pulseaudio.enable = false;
   hardware.bluetooth.enable = true; # enables support for Bluetooth
