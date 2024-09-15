186ff12fe154c520f285ef9eeb3243895b6bc1b0
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-09-15 12:24:16 -0600

    networking changes to try to get vpn working with update-systemd-resolved
    a few more nvidia tweaks to save power and processing

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -2,33 +2,53 @@
 
 {
   environment.systemPackages = with pkgs; [
-    update-systemd-resolved
     networkmanager
     networkmanager-openvpn
+    update-systemd-resolved
   ];
 
   systemd.services.systemd-resolved.environment = with lib; {
-    LD_LIBRARY_PATH = "${getLib pkgs.libidn2}/lib";
+    LD_LIBRARY_PATH = "${getLib pkgs.libidn2}/lib:$LD_LIBRARY_PATH";
+  };
+
+  services.openvpn.servers = {
+    straker_vpn = {
+      config = ''
+        config /home/jimh/work/straker_vpn.ovpn
+        auth-user-pass /home/jimh/work/auth.txt
+        auth-nocache
+        script-security 2
+        up ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved
+        up-restart
+        down ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved
+        down-pre
+      '';
+      autoStart = false;
+    };
+  };
+
+  networking = {
+    firewall = {
+      enable = true;
+      checkReversePath = "loose";
+      allowedTCPPorts = [
+        80
+        443
+      ];
+    };
+
+    networkmanager = {
+      enable = true;
+    };
   };
 
   services.resolved = {
     enable = true;
-  };
-
-  networking.networkmanager = {
-    enable = true;
-  };
-
-  # Open ports in the firewall.
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [
-      631
-      53
-    ];
-    allowedUDPPorts = [
-      5353
-      111
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
     ];
   };
 
