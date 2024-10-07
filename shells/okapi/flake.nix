b8d558c2a8592b23ae39b84b6eb78a2d8361a8f0
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-10-07 12:57:28 -0600

    small unsuccesful tweaks to okapi shell

--- shells/okapi/flake.nix
+++ shells/okapi/flake.nix
@@ -45,13 +45,18 @@
           export GSETTINGS_SCHEMA_DIR="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas/"
           export XDG_DATA_DIRS="$GSETTINGS_SCHEMA_DIR:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/:$XDG_DATA_DIRS"
           export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.webkitgtk}/lib:${pkgs.glib}/lib:${pkgs.glib-networking}/lib:${pkgs.swt}/lib:${pkgs.gtk3}/lib"
-          #export SWT_GTK3=1
           export GI_TYPELIB_PATH="${pkgs.gobject-introspection}/lib/girepository-1.0"
           export XDG_DATA_DIRS="${pkgs.shared-mime-info}/share:${pkgs.hicolor-icon-theme}/share:$XDG_DATA_DIRS"
           export GTK_USE_PORTAL=1
-          export GTK2_RC_FILES="${pkgs.kdePackages.breeze-gtk}/share/themes/Breeze/gtk-2.0/gtkrc"
           export GTK_THEME="Breeze"
-          export SWT_LIB_DIR="${pkgs.swt}/lib" # Set SWT library directory
+          export GTK2_RC_FILES="${pkgs.kdePackages.breeze-gtk}/share/themes/Breeze/gtk-2.0/gtkrc"
+          
+          # Add SWT libraries to java.library.path
+          export _JAVA_OPTIONS="$_JAVA_OPTIONS -Djava.library.path=${pkgs.swt}/lib"
+          
+          # Ensure SWT can find its native libraries
+          export SWT_GTK3=1
+          export SWT_LIBRARY_PATH="${pkgs.swt}/lib"
         '';
       };
     };
