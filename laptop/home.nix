336a7201a66fb719c50cb258e555216ea9bfa098
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-10-18 23:29:45 -0600

    update

--- laptop/home.nix
+++ laptop/home.nix
@@ -60,7 +60,7 @@
     shellAliases = {
       flake-check = "nix flake check --verbose --show-trace ${flakeDir}";
       flake-rebuild = "sudo nixos-rebuild switch --keep-going --flake ${flakeDir}#laptop";
-      flake-update = "sudo nix flake update ${flakeDir}";
+      flake-update = "sudo nix flake update --flake ${flakeDir}";
       gcCleanup = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       less = "most";
       cat = "bat";
