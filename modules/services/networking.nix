a1015d8ded05f77dd6088162861a2fee55551297
Jim Hargrave <jim.hargrave@strakergroup.com>
2025-06-17 21:48:59 -0600

    updates to networking to try to resolve PIA DNS failure

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -11,12 +11,12 @@
     LD_LIBRARY_PATH = "${getLib pkgs.libidn2}/lib:$LD_LIBRARY_PATH";
   };
 
-  networking.wireguard.enable = true;
-
   networking = {
+    wireguard.enable = true;
+    
     firewall = {
       enable = true;
-      checkReversePath = "loose";
+      checkReversePath = false;
       allowedTCPPorts = [
         80
         443
@@ -25,22 +25,31 @@
       allowedUDPPorts = [
         53
         51820
+        1317 # PIA client additional UDP port
       ];
-      # Trust traffic from the PIA interface
+      # Trust traffic from the PIA interface and allow DNS and PIA communication
       extraCommands = ''
         iptables -A INPUT -i wgpia+ -j ACCEPT
+        iptables -A OUTPUT -o wgpia+ -p udp --dport 53 -j ACCEPT
+        iptables -A OUTPUT -o wgpia+ -p udp --dport 1317 -j ACCEPT
       '';
     };
 
-    hostName = "datalore";
-    domain = "local";
     networkmanager = {
       enable = true;
+      dns = "systemd-resolved"; # Let NetworkManager manage DNS dynamically
     };
-    # Set the system's DNS resolver to our local unbound instance.
-    nameservers = [ "192.168.50.1" ];
+  };
+
+  services.resolved = {
+    enable = true;
+    dnssec = "allow-downgrade"; # Compatible with PIA's DNS
+    fallbackDns = [
+      "192.168.50.1" # Router DNS
+      "1.1.1.1"      # Cloudflare DNS
+      "1.0.0.1"      # Cloudflare DNS
+    ];
   };
 
   networking.timeServers = [ "pool.ntp.org" ];
-
 }
