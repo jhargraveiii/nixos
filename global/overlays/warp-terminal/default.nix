45f6b2f267149cad773812c9476c4227dbd09291
Jim Hargrave <jim.hargrave@strakergroup.com>
2025-06-30 11:46:16 -0600

    upgrade warp

--- global/overlays/warp-terminal/default.nix
+++ global/overlays/warp-terminal/default.nix
@@ -2,12 +2,13 @@
 self: super: {
   warp-terminal = super.warp-terminal.overrideAttrs (oldAttrs: rec {
     pname = "warp-terminal";
-    version = "0.2025.06.20.22.47.stable_05";
+    version = "0.2025.06.25.08.12.stable_01";
 
     src = super.fetchurl {
+      # https://releases.warp.dev/stable/v0.2025.06.25.08.12.stable_01/warp-terminal-v0.2025.06.25.08.12.stable_01-1-x86_64.pkg.tar.zst
       url = "https://releases.warp.dev/stable/v${version}/warp-terminal-v${version}-1-x86_64.pkg.tar.zst";
       # This is the correct hash that Nix found.
-      hash = "sha256-yrwS6rqSGkiWNjr17MVyH+ZQL2CTUqt6coi8qWfq0Gg=";
+      hash = "sha256-2VQzEBAAu6YasottOTu3YjQvvXR5gT3DAjGhp63wMNo=";
     };
   });
 }
