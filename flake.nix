ee69dde9ce297ff031c0b27dbef29e9c0c36b20a
Jim Hargrave <jim.hargrave@strakergroup.com>
2025-11-26 17:30:03 -0700

    match nix and HM versions

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nil = {
